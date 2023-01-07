25196065edfc28f845ee9e62a44b484527e15fcd
bowersjuan <bowersjuan@gmail.com>
2023-01-06 22:13:34 -0500

    install uuid package and implement into transaction model

--- models/transaction.js
+++ models/transaction.js
@@ -1,5 +1,8 @@
+const { v4: uuidv4 } = require("uuid");
+
 module.exports = [
   {
+    id: uuidv4(),
     itemName: "monthly tips",
     amount: 1000,
     date: "2023-1-1",
@@ -7,6 +10,7 @@ module.exports = [
     category: "income",
   },
   {
+    id: uuidv4(),
     itemName: "food",
     amount: -200,
     date: "2023-1-8",
